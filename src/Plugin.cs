0bc036293be984eea21c792b3941f7704ac746d5
WatcherMagic <oliviaw.randomnumber@gmail.com>
2024-11-16 15:26:55 -0700

    Add project files.

---
+++ src/Plugin.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using BepInEx;
+using UnityEngine;
+using SlugBase.Features;
+using static SlugBase.Features.FeatureTypes;
+
+namespace SlugTemplate
+{
+    [BepInPlugin(MOD_ID, "Evil McEvilslug", "0.1.0")]
+    class Plugin : BaseUnityPlugin
+    {
+        private const string MOD_ID = "mcevilslug";
+
+        // Add hooks
+        public void OnEnable()
+        {
+            
+        }
+        
+        // Load any resources, such as sprites or sounds
+        private void LoadResources(RainWorld rainWorld)
+        {
+        }
+    }
+}
